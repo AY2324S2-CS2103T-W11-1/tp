db6007348d68ff575b93407ec39a8748c4f5b823
theman-oj10 <manojnarender@gmail.com>
2024-03-02 03:18:05 +0800

    Fixed Checkstyle

--- src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -9,6 +9,9 @@ import seedu.address.logic.commands.RemarkCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Remark;
 
+/**
+ * Parses input arguments and creates a new RemarkCommand object
+ */
 public class RemarkCommandParser {
     static final Prefix PREFIX_REMARK = new Prefix("r/");
     /**
